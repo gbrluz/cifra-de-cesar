0c444512c47d77e77b614cee627dcd0f2057922c
gabrielluz <gabriel-h-luz@hotmail.com>
2022-04-19 12:34:17 -0300

    first commit

---
+++ src/main/java/com/example/cifradecesar/CifraDeCesarApplication.java
@@ -0,0 +1,51 @@
+package com.example.cifradecesar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.text.Normalizer;
+import java.util.*;
+
+@SpringBootApplication
+public class CifraDeCesarApplication {
+
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(CifraDeCesarApplication.class, args);
+
+        Scanner scanner = new Scanner(System.in);
+
+        String alfabeto = "abcdefghijklmnopqrstuvwxyz";
+        char[] letras = alfabeto.toCharArray();
+
+        System.out.println("Insira a string desejada: ");
+        char[] palavra = scanner.nextLine().toLowerCase().replaceAll("\\p{Punct}", "")
+                .replaceAll("á|à|â|ã|ä", "a")
+                .replaceAll("é|è|ê|ë", "e").replaceAll("Ì|Í|Î|Ï", "i")
+                .replaceAll("Ò|Ó|Ô|Õ|Ö", "o").replaceAll("Ù|Ú|Û|Ü", "u")
+                .toCharArray();
+        System.out.println("Insira a chave desejada: ");
+        int chave = scanner.nextInt();
+
+        int tam = palavra.length;
+        int az = letras.length;
+
+        for (int i = 0; i < tam; i++) {
+            for (int j = 0; j < az; j++) {
+                if (palavra[i] == letras[j]) {
+                    while ((chave+j)>25){
+                        chave -= 26;
+                    }
+                    while ((chave+j)<0){
+                        chave += 26;
+                    }
+                    palavra[i] = letras[j + chave];
+                    break;
+                }
+            }
+        }
+        System.out.print("A frase codificada é: ");
+        System.out.println(palavra);
+    }
+}
---
+++ src/test/java/com/example/cifradecesar/CifraDeCesarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cifradecesar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CifraDeCesarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
